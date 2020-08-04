ae33eb30cafa60e831bde2d2f657e73263658cce
zerocy18 <54656903+zerocy18@users.noreply.github.com>
2020-08-04 14:58:52 +0900

    EventPractice

---
+++ eventPractice/App.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import EventPractice from "./EventPractice";
+
+class App extends Component {
+  render() {
+    return <EventPractice />;
+  }
+}
+
+export default App;
