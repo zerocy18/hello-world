9fb4c401d747721620d7f8595e41f1ea1f194bdd
zerocy18 <54656903+zerocy18@users.noreply.github.com>
2020-08-04 19:23:52 +0900

    Create App.js

---
+++ ValidationSample/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+//import EventPractice from "./EventPractice";
+import ValidationSample from "./ValidationSample";
+
+class App extends Component {
+  render() {
+    return <ValidationSample />;
+  }
+}
+
+export default App;
